2a23897f8202bf17393d379d80feac3ed94e1b0d
Iván Sánchez Míguez <ivasan07@ucm.es>
2023-08-02 20:13:24 +0200

    class component and attributes

--- GUI/src/ImGUI/Component.h
+++ GUI/src/ImGUI/Component.h
@@ -1,13 +1,40 @@
 #pragma once
 #include <iostream>
+#include "imgui.h"
 
-enum  componentType { transform };
+enum AttributesType {
+	INT,
+	FLOAT,
+	VECTOR2,
+	STRING,
+	BOOL,
+	//COLOR,
+};
+
+class Attribute {
+	AttributesType type;
+	std::string name;
+
+	union value
+	{
+		int valueInt;
+		float valueFloat;
+		ImVec2 valueVector2;
+		char* valueString;
+		bool valueBool;
+		//color
+	};
+
+};
 
 class Component
 {
 public:
-	componentType type;
-	virtual void drawEditor() = 0;
-	virtual std::string toJson() = 0;
+	std::string name;
+
+	std::unordered_map<std::string, Attribute> attributes;
+
+	virtual void drawEditor();
+	virtual std::string toJson();
 };
 
