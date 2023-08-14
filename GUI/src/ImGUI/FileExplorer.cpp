afdae881c0b7a2c68b0a714160dea85afd3bafc5
Yojhan <yojhansg@ucm.es>
2023-08-14 18:30:36 +0200

    Cambiado el file explorer para ordenar por carpetas

--- GUI/src/ImGUI/FileExplorer.cpp
+++ GUI/src/ImGUI/FileExplorer.cpp
@@ -18,146 +18,169 @@ PEditor::FileExplorer::FileExplorer() : Window("FileExplorer", NoResize | NoColl
 
 	ImVec2 windowSize = ImVec2(mainWindowSize.x, 297 * mainWindowSize.y / 1080);
 
-    windowOriWidth = windowSize.x;
-    windowOriHeight = windowSize.y;
+	windowOriWidth = windowSize.x;
+	windowOriHeight = windowSize.y;
 
 	setSize(ImVec2(windowSize.x, windowSize.y));
 	setPosition(ImVec2(0, 784 * mainWindowSize.y / 1080));
 
-    windowOriPosX = windowPosX;
-    windowOriPosY = windowPosY;
+	windowOriPosX = windowPosX;
+	windowOriPosY = windowPosY;
 
-    char buffer[FILENAME_MAX];
-    _getcwd(buffer, FILENAME_MAX);
-    projectPath = buffer;
-    currentPath = projectPath;
+	char buffer[FILENAME_MAX];
+	_getcwd(buffer, FILENAME_MAX);
+	projectPath = buffer;
+	currentPath = projectPath;
 
-    std::string imgPath = "folder.png";
-    SDL_Surface* surface = IMG_Load(imgPath.c_str());
-    folderTexture = SDL_CreateTextureFromSurface(ImGUIManager::getInstance()->getRenderer(), surface);
+	std::string imgPath = "folder.png";
+	SDL_Surface* surface = IMG_Load(imgPath.c_str());
+	folderTexture = SDL_CreateTextureFromSurface(ImGUIManager::getInstance()->getRenderer(), surface);
 
-    imgPath = "file.png";
-    surface = IMG_Load(imgPath.c_str());
-    fileTexture = SDL_CreateTextureFromSurface(ImGUIManager::getInstance()->getRenderer(), surface);
+	imgPath = "file.png";
+	surface = IMG_Load(imgPath.c_str());
+	fileTexture = SDL_CreateTextureFromSurface(ImGUIManager::getInstance()->getRenderer(), surface);
 
-    imgPath = "script.png";
-    surface = IMG_Load(imgPath.c_str());
-    scriptTexture = SDL_CreateTextureFromSurface(ImGUIManager::getInstance()->getRenderer(), surface);
+	imgPath = "script.png";
+	surface = IMG_Load(imgPath.c_str());
+	scriptTexture = SDL_CreateTextureFromSurface(ImGUIManager::getInstance()->getRenderer(), surface);
 
 
 
-    SDL_FreeSurface(surface);
+	SDL_FreeSurface(surface);
 }
 
 void PEditor::FileExplorer::drawFileExplorerWindow()
 {
 
-    // Open the specified folder
-    fs::path folder(currentPath);
+	// Open the specified folder
+	fs::path folder(currentPath);
 
 
-    if (!fs::is_directory(folder))
-    {
-        ImGui::Text("Invalid folder path: %s", currentPath);
-        ImGui::End();
-        return;
-    }
+	if (!fs::is_directory(folder))
+	{
+		ImGui::Text("Invalid folder path: %s", currentPath);
+		ImGui::End();
+		return;
+	}
 
-    ImGui::Text("Folder: %s", currentPath.c_str());
+	ImGui::Text("Folder: %s", currentPath.c_str());
 
-    if (currentPath != projectPath) {
+	if (currentPath != projectPath) {
 
-        // Display buttons to navigate up and down the folder hierarchy
-        ImGui::SameLine();
-        if (ImGui::Button("^"))
-        {
-            // Navigate to parent folder
-            currentPath = folder.parent_path().string();
-        }
-    }
+		// Display buttons to navigate up and down the folder hierarchy
+		ImGui::SameLine();
+		if (ImGui::Button("^"))
+		{
+			// Navigate to parent folder
+			currentPath = folder.parent_path().string();
+		}
+	}
 
-    ImGui::Separator();
+	ImGui::Separator();
 
-    // Display a list of files and directories in the folder
-    for (auto& file : fs::directory_iterator(folder))
-    {
-        std::string filename = file.path().filename().string();
+	// Display a list of all directories in the folder
+	for (auto& file : fs::directory_iterator(folder))
+	{
+		if (file.is_directory())
+		{
+			std::string filename = file.path().filename().string();
 
-        ImTextureID texture = file.is_directory() ? folderTexture : fileTexture;
+			ImTextureID texture = file.is_directory() ? folderTexture : fileTexture;
 
-        if (file.path().extension().string() == ".script") texture = scriptTexture;
+			if (file.path().extension().string() == ".script") texture = scriptTexture;
 
 
 
-        const float iconSize = ImGui::GetTextLineHeight() + 8;
-        ImGui::Image(texture, ImVec2(iconSize, iconSize), ImVec2(0.0f, 0.0f), ImVec2(1.0f, 1.0f));
+			const float iconSize = ImGui::GetTextLineHeight() + 8;
+			ImGui::Image(texture, ImVec2(iconSize, iconSize), ImVec2(0.0f, 0.0f), ImVec2(1.0f, 1.0f));
 
-        ImGui::SameLine();
+			ImGui::SameLine();
 
-        ImGui::SetWindowFontScale(1.5);
+			ImGui::SetWindowFontScale(1.5);
 
-        if (file.is_directory())
-        {
-            ImGui::PushStyleColor(ImGuiCol_Text, ImVec4(0.65f, 0.65f, 1.0f, 1.0f)); 
+			ImGui::PushStyleColor(ImGuiCol_Text, ImVec4(0.65f, 0.65f, 1.0f, 1.0f));
 
-            // Display directories in blue color
-            if (ImGui::Selectable(filename.c_str(), false))
-            {
-                currentPath = file.path().string();
-            }
+			// Display directories in blue color
+			if (ImGui::Selectable(filename.c_str(), false))
+			{
+				currentPath = file.path().string();
+			}
 
-            ImGui::PopStyleColor(1);
-        }
-        else
-        {
-            // Display files in default color
-            ImGui::Text(filename.c_str());
-            std::string extension = file.path().extension().string();
+			ImGui::PopStyleColor(1);
+		}
 
-            if (extension == ".png" || extension == ".jpg")
-            {
-                ImGui::SameLine();
-                std::string relativePath = file.path().lexically_relative(projectPath).string();
-                std::string buttonId = "Create GameObject##" + relativePath;
-                if (ImGui::Button(buttonId.c_str())) {
-                    ImGUIManager::getInstance()->getScene()->addGameObject(relativePath);
-                }
-            }
 
-            if (extension == ".script")
-            {
-                ImGui::SameLine();
-                std::string buttonId = "Open script##" + filename;
-                if (ImGui::Button(buttonId.c_str())) {
-                    ImGui::OpenPopup("Create script");
+		ImGui::SetWindowFontScale(1);
+	}
 
-                    size_t dotPos = filename.find_last_of(".");
-                    std::string filenameWithoutExtension = filename.substr(0, dotPos);
+	// Display a list of all files in the folder
+	for (auto& file : fs::directory_iterator(folder))
+	{
+		if (!file.is_directory())
+		{
+			std::string filename = file.path().filename().string();
 
-                    ImGUIManager::getInstance()->getScriptCreation()->SetName(filenameWithoutExtension);
-                    ImGUIManager::getInstance()->changeEditorState(ImGUIManager::EDITOR_STATE::SCRIPTING_WINDOW);
-                }
-            }
-           
-        }
+			ImTextureID texture = file.is_directory() ? folderTexture : fileTexture;
 
-        ImGui::SetWindowFontScale(1);
-    }
+			if (file.path().extension().string() == ".script") texture = scriptTexture;
+
+
+
+			const float iconSize = ImGui::GetTextLineHeight() + 8;
+			ImGui::Image(texture, ImVec2(iconSize, iconSize), ImVec2(0.0f, 0.0f), ImVec2(1.0f, 1.0f));
+
+			ImGui::SameLine();
+
+			ImGui::SetWindowFontScale(1.5);
+
+			// Display files in default color
+			ImGui::Text(filename.c_str());
+			std::string extension = file.path().extension().string();
+
+			if (extension == ".png" || extension == ".jpg")
+			{
+				ImGui::SameLine();
+				std::string relativePath = file.path().lexically_relative(projectPath).string();
+				std::string buttonId = "Create GameObject##" + relativePath;
+				if (ImGui::Button(buttonId.c_str())) {
+					ImGUIManager::getInstance()->getScene()->addGameObject(relativePath);
+				}
+			}
+
+			if (extension == ".script")
+			{
+				ImGui::SameLine();
+				std::string buttonId = "Open script##" + filename;
+				if (ImGui::Button(buttonId.c_str())) {
+					ImGui::OpenPopup("Create script");
+
+					size_t dotPos = filename.find_last_of(".");
+					std::string filenameWithoutExtension = filename.substr(0, dotPos);
+
+					ImGUIManager::getInstance()->getScriptCreation()->SetName(filenameWithoutExtension);
+					ImGUIManager::getInstance()->changeEditorState(ImGUIManager::EDITOR_STATE::SCRIPTING_WINDOW);
+				}
+			}
+
+		}
+
+		ImGui::SetWindowFontScale(1);
+	}
 
 }
 
 void PEditor::FileExplorer::render()
 {
 
-    // Draw the file explorer
-    ImGui::Begin(windowName.c_str(), (bool*)0, (ImGuiWindowFlags_)flags);
+	// Draw the file explorer
+	ImGui::Begin(windowName.c_str(), (bool*)0, (ImGuiWindowFlags_)flags);
 
-    ImGui::SetWindowSize(ImVec2(windowWidth, windowHeight));
+	ImGui::SetWindowSize(ImVec2(windowWidth, windowHeight));
 
-    ImGui::SetWindowPos(ImVec2(windowPosX, windowPosY));
+	ImGui::SetWindowPos(ImVec2(windowPosX, windowPosY));
 
-    drawFileExplorerWindow();
+	drawFileExplorerWindow();
 
-    ImGui::End();
+	ImGui::End();
 
 }
